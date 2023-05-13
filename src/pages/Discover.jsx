c536a842dd25ed0d6f2ab76e3b2eb505bce4cc8f
vaisakh <vaisakhsindhu11@gmail.com>
2023-05-13 16:39:38 +0530

    error and loader component added

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -4,9 +4,9 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
-  console.log(data)
-  console.log(error)
   const genreTitle = "Pop";
+  if (isFetching) return <Loader title="loading songs..." />;
+  if (error) return <Error/>
   return (
     <div className="flex flex-col">
       <div
@@ -31,8 +31,8 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
-          <SongCard key={song} song={song} i={i} />
+        {data?.tracks.map((song, i) => (
+          <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
     </div>
