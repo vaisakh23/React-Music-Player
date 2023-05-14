6b5bef1e32d3e4d38dddd799ea8eac5c57348ee8
vaisakh <vaisakhsindhu11@gmail.com>
2023-05-14 21:11:26 +0530

    work on playPause component

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,8 +3,9 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => {
-  const activeSong = "test";
+const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
+  const handlePauseClick = () => {};
+  const handlePlayClick = () => {};
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
@@ -16,7 +17,13 @@ const SongCard = ({ song, i }) => {
               : "hidden"
           }`}
         >
-          <PlayPause />
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img src={song.images?.coverart} alt="song_img" />
       </div>
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
   if (isFetching) return <Loader title="loading songs..." />;
@@ -32,7 +36,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.tracks.map((song, i) => (
-          <SongCard key={i} song={song} i={i} />
+          <SongCard
+            key={i}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            i={i}
+            data={data}
+          />
         ))}
       </div>
     </div>
