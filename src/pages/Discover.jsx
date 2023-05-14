469b07ce64da481812ef7c4e5e19750393a6f3c2
vaisakh <vaisakhsindhu11@gmail.com>
2023-05-14 18:03:09 +0530

    songcard component disign - song conver title artists

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,7 +6,7 @@ const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
   if (isFetching) return <Loader title="loading songs..." />;
-  if (error) return <Error/>
+  if (error) return <Error />;
   return (
     <div className="flex flex-col">
       <div
@@ -32,7 +32,7 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.tracks.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard key={i} song={song} i={i} />
         ))}
       </div>
     </div>
